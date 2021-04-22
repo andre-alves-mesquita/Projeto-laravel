a2ac6d5ce35965b82abdccc9cd51005da7318a5b
andre a <andre20mesquita@gmail.com>
2021-04-22 15:42:58 -0300

    segundo commit

--- resources/views/temporadas/index.blade.php
+++ resources/views/temporadas/index.blade.php
@@ -9,7 +9,7 @@ Temporadas de {{ $serie->nome }}
     @foreach($temporadas as $temporada)
     <li class="list-group-item d-flex justify-content-between align-items-center">
         <a href="">Temporada {{ $temporada->numero }}</a>
-        <span class="badge bg-secondary">0/{{$temporada->episodios->count()}}</span>
+        <span class="badge bg-secondary">5/{{$temporada->episodios->count()}}</span>
     </li>
 
     @endforeach
